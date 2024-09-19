857bd76701b0eb44895be708aa2c4f98687fcb49
Shreekrishna T <124446964+TShreek@users.noreply.github.com>
2024-09-19 13:12:31 +0530

    Update largestNumber.cpp

--- cpp/largestNumber.cpp
+++ cpp/largestNumber.cpp
@@ -4,32 +4,23 @@ Since the result may be very large, so you need to return a string instead of an
   */
 class Solution {
 public:
-    // Custom comparator function
-    static bool compare(string& a, string& b) {
-        return a + b > b + a;
+    static bool compare(string a, string b){
+        return a+b > b+a;
     }
-
     string largestNumber(vector<int>& nums) {
-        // Convert all integers to strings
-        vector<string> nums_str;
-        for (int num : nums) {
+       vector<string> nums_str;
+        for(int& num : nums){
             nums_str.push_back(to_string(num));
         }
-
-        // Sort the strings using the custom comparator
-        sort(nums_str.begin(), nums_str.end(), compare);
-
-        // Edge case: if the largest number is '0', return '0'
-        if (nums_str[0] == "0") {
+        
+        sort(nums_str.begin(),nums_str.end(),compare);
+        
+        if(nums_str[0] == "0")
             return "0";
-        }
-
-        // Concatenate the sorted strings to form the largest number
         string result;
-        for (string& num_str : nums_str) {
-            result += num_str;
+        for( string& num : nums_str){
+            result += num;
         }
-
         return result;
     }
 };
