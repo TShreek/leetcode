83307a7d455794bba60105be4774ba32877e4bf7
Shreekrishna T <124446964+TShreek@users.noreply.github.com>
2025-08-30 17:10:43 +0530

    Create SortList.cpp

---
+++ top150/SortList.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(!head || !head->next) return head;
+
+        ListNode* slow = head, *fast = head, *prev = nullptr;
+        while(fast && fast->next){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = nullptr;
+        ListNode* l1 = sortList(head);
+        ListNode* l2 = sortList(slow);
+
+        return merge(l1,l2);
+    }
+private :
+    ListNode* merge(ListNode* l1, ListNode* l2){
+        ListNode dummy(0);
+        ListNode* temp = &dummy;
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                temp->next = l1;
+                l1 = l1->next;
+            } else{
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            temp = temp->next;
+        }
+        temp->next = l1? l1 : l2;
+        return dummy.next;
+    }
+};
